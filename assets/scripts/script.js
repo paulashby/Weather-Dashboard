f8cb95146663cd9db943e4bf863e154ac56cdba6
Paul Ashby <paul@primitive.co>
2022-12-21 14:47:35 +0000

    Refine history, search and status functionality Load data for city when history buttons are clicked. Rename updateHistory function as updateHistoryBtns. Move history update to its own, new updateHistory function. Use lat/long/city vars in outer (document.ready) scope. Use show-status class to toggle visibilty of status messages. Clear search input on focus. Trim and lowercase user input for data consistency. Where available, use city name from geolocation on page load.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,11 +5,14 @@ $(document).ready(function () {
     var geoCodeSegment = "geo/1.0/direct";
     var geoCodeEnd = "&limit=1&appid=" + API_KEY;
     var forecastSegment = "data/2.5/forecast";
+    var body = $("body");
     var todayElmt = $("#today");
     var fiveDayForecastElmt = $("#five-day-forecast");
     var historyElmt = $("#history");
+    var statusMessageElmt = $("#message");
+    var geolocated = false;
 
-    updateHistory();
+    updateHistoryBtns();
 
     // Default to London
     var city = "London";
@@ -17,67 +20,64 @@ $(document).ready(function () {
     var lon = -0.1276474;
 
     if ('geolocation' in navigator) {
-        todayElmt.html("<h2 class='col-12'>Loading weather data...</h2>");
+        statusMessageElmt.text("Loading weather data...");
+        body.addClass("show-status");
         navigator.geolocation.getCurrentPosition(function (position) {
+            // City name will be set according to geolocation data
+            geolocated = true;
             // Location acquired - update lat/lon and call with new values
             lat = position.coords.latitude;
             lon = position.coords.longitude;
-            updateWeatherData(lat, lon);
+            updateWeatherData();
         }, function () {
             // Error getting location - call with default values
-            updateWeatherData(lat, lon); 
+            updateWeatherData();
         });
     } else {
         // Geolocation not available - call with defualt values
-        updateWeatherData(lat, lon);  
-    }  
+        updateWeatherData();
+    }
 
+    $(document).on('click', '.btn-history', handleHistoryClick);
     $("#search-form").on("submit", handleSearch);
+    $("#search-input").on("focus", function(){
+        $(this).val("");
+    });
 
     function handleSearch(event) {
 
         event.preventDefault();
 
-        city = $("input[id='search-input']").val();
+        city = $("input[id='search-input']").val().trim().toLowerCase();
+        loadCityData();
+    }
 
-        // Add to history
-        var history = JSON.parse(window.localStorage.getItem("history")) || [];
+    function handleHistoryClick(event) {
+        city = $(this).attr("data-city");
+        loadCityData();
+    }
 
-        // Remove previous entry for this city so searches remain chronological
-        history = history.filter(function (item) {
-            return item !== city;
-        });
+    function loadCityData() {
 
-        // Add latest search to updated History
-        history.push(city);
+        var geoQueryURL = baseURL + geoCodeSegment + "?q=" + city + geoCodeEnd;
 
-        // Update in local storage
-        window.localStorage.setItem("history", JSON.stringify(history));
+        // Get lat/lon based on city name
+        $.ajax({
+            url: geoQueryURL,
+            method: "GET"
+        })
+            .then(function (response) {
 
-        // Update DOM
-        updateHistory();
+                // Show error if no city returned
+                if (!response.length) {
+                    return showNoDataAlert(city);
+                }
+                // Get weather data using updated lat/lon
+                lat = response[0].lat;
+                lon = response[0].lon;
+                updateWeatherData();
 
-
-        if (city) {
-            var geoQueryURL = baseURL + geoCodeSegment + "?q=" + city + geoCodeEnd;
-
-            $.ajax({
-                url: geoQueryURL,
-                method: "GET"
-            })
-                .then(function (response) {
-
-                    // Show error if no city returned
-                    if (!response.length) {
-                        return showNoDataAlert(city);
-                    }
-
-                    var lat = response[0].lat;
-                    var lon = response[0].lon;
-                    updateWeatherData(lat, lon);
-
-                });
-        }
+            });
     }
 
     // Remove results from previous calls
@@ -121,12 +121,15 @@ $(document).ready(function () {
     }
 
     function showNoDataAlert(city) {
-        fiveDayForecastElmt.html("<div class='col-12'><div class='alert alert-danger' role='alert'>No data available for " + city + ".</div></div>");
+        statusMessageElmt.text("No data available for " + city + ".");
+        body.addClass("show-status");
     }
 
-    function updateWeatherData(lat, lon) {
+    function updateWeatherData() {
 
         var forecastURL = baseURL + forecastSegment + "?lat=" + lat + "&lon=" + lon + "&appid=" + API_KEY;
+        // Remove "loading" message
+        body.removeClass("show-status");
 
         $.ajax({
             url: forecastURL,
@@ -136,11 +139,16 @@ $(document).ready(function () {
 
                 clearResults();
 
-                // Update city for Geolocation-based calls
-                city = response.city.name;
+                if (geolocated) {
+                    // Update city for Geolocation-based calls
+                    city = response.city.name;
+                    geolocated = false;
+                }
+
                 // Store forecast data for convenience
                 var forecasts = response.list;
-                // Store time of first forecast
+
+                // Get time of first forecast
                 var forecastTime = getHour(forecasts[0].dt);
 
                 // Filter out all other forecast times
@@ -149,17 +157,18 @@ $(document).ready(function () {
                 });
                 var numDays = dailyForecasts.length;
 
-                if (!numDays) {
+                if (numDays) {
+                    updateHistory();
+                } else {
                     // No data available - show alert
                     return showNoDataAlert(city);
-
                 }
 
-                for (var i = 0; i < numDays; i++) {                    
+                for (var i = 0; i < numDays; i++) {
                     var currDay = dailyForecasts[i];
                     var conditionsList = formatConditions(currDay);
                     var date = formatDate(currDay.dt);
-
+                    // Update DOM
                     var currDayTitle = $("<h4 class='card-title'>").text(date);
                     var currDayForecast = $("<div class='col-12 col-sm-6 col-md-4 col-xl mb-4'>")
                         .append($("<div class='card rounded-0 long-forecast-entry'>")
@@ -179,15 +188,34 @@ $(document).ready(function () {
             });
     }
 
-    function updateHistory() {
+    function updateHistoryBtns() {
 
         historyElmt.empty();
 
         var history = JSON.parse(window.localStorage.getItem("history")) || [];
 
         for (var i = 0; i < history.length; i++) {
-            var cityBtn = $("<button class='btn btn-secondary mb-2' type='submit'>").attr("data-city", history[i]).text(history[i]);
+            var cityBtn = $("<button class='btn btn-secondary mb-2 btn-history' type='submit'>").attr("data-city", history[i]).text(history[i]);
             historyElmt.prepend(cityBtn);
         }
+    }
+
+    function updateHistory() {
+        // Get existing history data
+        var history = JSON.parse(window.localStorage.getItem("history")) || [];
+
+        // Remove previous entry for this city so searches remain chronological
+        history = history.filter(function (item) {
+            return item !== city;
+        });
+
+        // Add latest search to updated History
+        history.push(city);
+
+        // Update in local storage
+        window.localStorage.setItem("history", JSON.stringify(history));
+
+        // Update DOM
+        updateHistoryBtns();
     }
 });
